90d33ecb19a76292330f8cbd26d4e2c00984d7d8
saptarshi chowdhury <142312774+1saptarshi@users.noreply.github.com>
2025-05-28 22:38:25 +0530

    add algo in find_element_index

---
+++ Find_element_index.py
@@ -0,0 +1,8 @@
+# # ✅ 1. Brute Force Approach
+# 🔧 Algorithm:
+# Store the first element in a temporary variable.
+
+# Shift all elements from index 1 to N-1 to the left by one.
+
+# Put the stored element at the end of the array.
+

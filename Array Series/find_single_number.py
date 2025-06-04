1a3943a3afc7534effb58a5e029cbdb88932c98f
saptarshi chowdhury <142312774+1saptarshi@users.noreply.github.com>
2025-06-04 10:05:00 +0530

    find single number

---
+++ Array Series/find_single_number.py
@@ -0,0 +1,12 @@
+def find_single_number(arr):
+    result = 0
+    for num in arr:
+        result ^= num
+    return result
+
+# Example
+arr = [2, 3, 5, 4, 5, 3, 2]
+print("The single number is:", find_single_number(arr))  # Output: 4
+
+
+ 
